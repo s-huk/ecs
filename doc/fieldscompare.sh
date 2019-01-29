982d5f747c9d016dc4551652c4eeb1760329c63c
Felix Müller <f.mueller@avm.de>
2019-01-29 12:13:04 +0100

    nginx ECS-Pipeline v1

--- doc/fieldscompare.sh
+++ doc/fieldscompare.sh
@@ -2,4 +2,4 @@
 
 dir_of_script="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-curl -s https://raw.githubusercontent.com/elastic/ecs/master/fields.yml | diff -u --color "$dir_of_script/fields.yml" -
+curl -s https://raw.githubusercontent.com/elastic/ecs/master/fields.yml | diff -u "$dir_of_script/fields.yml" -
